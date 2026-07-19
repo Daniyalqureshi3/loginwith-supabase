48819e2716e49841be8197b90cef5997271a0f74
Muhammad Daniyal <danyalali607@gmail.com>
2026-07-19 11:26:41 +0500

    final update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = ''
-const supabaseKey = ''
+const supabaseUrl = 'https://idzumvvnofpgcwnllzff.supabase.co'
+const supabaseKey = 'sb_publishable_cvZlBf5r_ZFeiXBc9T5svA_wBs9zyJh'
 //  must use env
 export const supabase = createClient(supabaseUrl, supabaseKey)
